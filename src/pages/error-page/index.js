6ba97229fe7fe1949dd6bb52fd37e43f10157269
Mariusz Ilijew <checkanica@gmail.com>
2023-07-26 15:33:43 +0200

    fix: footer and landing page video css adjustment for responsive design

--- src/pages/error-page/index.js
+++ src/pages/error-page/index.js
@@ -17,7 +17,9 @@ const HomePage = () => {
       <div className={styles.div}>
         <Header />
         <Title title={<MultiLingualContent contentID='notFound'/>} />
+        <div className={styles.container}>
         <img className={styles.img404} src={bunny404} alt="Virtual Assistant Not Found 404" />
+        </div>
         {/* <img src="https://scontent.cdninstagram.com/v/t51.2885-15/120888078_916624432200187_6173337947522019984_n.jpg?stp=dst-jpg_e35&_nc_ht=scontent.cdninstagram.com&_nc_cat=107&_nc_ohc=_aDjTzT8h6wAX825C48&edm=APs17CUBAAAA&ccb=7-5&ig_cache_key=MjQxNDEwMDUwMzkyNDUyNjIxNg%3D%3D.2-ccb7-5&oh=00_AfBigj6OEiTYSowgMnXJ6GPCF-ykcVfjqNfPq6fdUv9ZgQ&oe=64C51408&_nc_sid=10d13b" alt="Virtual Assistant Not Found 404" /> */}
         <Footer />
       </div>

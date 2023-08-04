f330734c694c2a1bd5ff7af26347d163d8966503
Mariusz Ilijew <checkanica@gmail.com>
2023-08-04 12:26:00 +0200

    fix: troubleshooting localStorage

--- src/App.js
+++ src/App.js
@@ -4,16 +4,14 @@ import LanguageContext from './context.js';
 const App = (props) => {
 
   //using localStorage, so that client doesn't need to select language everytime he enters from the same brouser.
-  // function localLanguage() {
-  //   const storedLanguage = localStorage.getItem("language")
-  //   return storedLanguage ? JSON.parse(storedLanguage) : "en"
-  // }
-  // console.log("localLanguage()   ", typeof(localLanguage()))
-  // const [language, setLanguage] = useState(localLanguage());
-  // console.log("language   ", typeof(language))
+  function localLanguage() {
+    if (typeof window === "undefined") return "de"
+    const storedLanguage = localStorage.getItem("language")
+    return storedLanguage ? JSON.parse(storedLanguage) : "en"
+  }
+  const [language, setLanguage] = useState(localLanguage());
 
-
-  const [language, setLanguage] = useState("en");
+  // const [language, setLanguage] = useState("en");
 
   const selectLanguage = (selectedLanguage) => {
     setLanguage(selectedLanguage);

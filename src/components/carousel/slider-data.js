8c9f56530fea557119a3d369c4a0811c07d675d7
Mariusz Ilijew <checkanica@gmail.com>
2023-09-06 14:54:37 +0200

    feat: start migrate media to s3 bucket

--- src/components/carousel/slider-data.js
+++ src/components/carousel/slider-data.js
@@ -8,32 +8,32 @@ const slider = [
   {
     title: "homeCarouselTitle1",
     description: "homeCarouselDescription1",
-    // url: "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-    url: rose
+    // url: rose,
+    url: 'https://charlie-va.s3.eu-central-1.amazonaws.com/images/carousel/1_rose_Virtual_Assistant.jpg'
   },
   {
     title: "homeCarouselTitle2",
     description: "homeCarouselDescription2",
-    // url: "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    url: sky
+    // url: sky,
+    url: 'https://charlie-va.s3.eu-central-1.amazonaws.com/images/carousel/2_sky_Virtual_Assistant.jpg'
   },
   {
     title: "homeCarouselTitle3",
     description: "homeCarouselDescription3",
-    // url: "https://images.unsplash.com/photo-1646615077267-97c6088b74d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=415&q=80"
-    url: tulips
+    // url: tulips,
+    url: 'https://charlie-va.s3.eu-central-1.amazonaws.com/images/carousel/3_tulips_Virtual_Assistant.jpg'
   },
   {
     title: "homeCarouselTitle4",
     description: "homeCarouselDescription4",
-    // url: "https://images.unsplash.com/photo-1612240498936-65f5101365d2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"
-    url: dandelion
+    // url: dandelion,
+    url:'https://charlie-va.s3.eu-central-1.amazonaws.com/images/carousel/4_dandelion_Virtual_Assistant.jpg'
   },
   {
     title: "homeCarouselTitle5",
     description: "homeCarouselDescription5",
-    // url: "https://images.unsplash.com/photo-1551024601-bec78aea704b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    url: stJulians
+    // url: stJulians, 
+    url: 'https://charlie-va.s3.eu-central-1.amazonaws.com/images/carousel/5_StJulians_Virtual_Assistant.jpg'
   },
 ]
 
--- src/components/video/index.js
+++ src/components/video/index.js
@@ -15,14 +15,15 @@ const VideoBackG = () => {
     return (
         <>
             <div className={styles.videoContainerFull} >
-                <video src={videoBacground} autoPlay loop muted />
+                {/* <video src={videoBacground} autoPlay loop muted /> */}
+                <video src='https://charlie-va.s3.eu-central-1.amazonaws.com/va_welcome_video_xs_hd.mp4' autoPlay loop muted />
             </div>
             <div className={styles.videoContainerHalf}>
                 {/* <div className={styles.videoLeft}> */}
-                    <video className={styles.videoLeft} src={videoBacground} autoPlay loop muted />
+                <video className={styles.videoLeft} src={videoBacground} autoPlay loop muted />
                 {/* </div> */}
                 {/* <div className={styles.videoRight}> */}
-                    <video className={styles.videoRight} src={videoBacground} autoPlay loop muted />
+                <video className={styles.videoRight} src={videoBacground} autoPlay loop muted />
                 {/* </div> */}
             </div>
         </>
